f3fe8fb73eaf11801905c6a0b6f38136a05394e6
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2020-06-08 15:52:33 +0100

    few changes

--- style.js
+++ style.js
@@ -1,11 +1,3 @@
-// Preloader script
-
-// setTimeout(function () {
-
-//     $('.loader-container').addClass('done');
-//     $('.progress').addClass('done');
-
-// }, 10000);
 
 var overlay = document.getElementById("overlay");
 
